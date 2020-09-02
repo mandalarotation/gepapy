50654ad240fe365f1820aca5ec378098d3de920d
mandalarotation <mandalarotation@gmail.com>
2020-09-02 22:31:56 +0000

    reparacion de de algnos fallos en la sintaaxis de los  kernels

--- gsc/single_machine.py
+++ gsc/single_machine.py
@@ -10,20 +10,20 @@ from gsc.kernels.mutationA0001 import MutationA0001
 
 class Single_Machine(PermutationA0001,CrossA0001,MutationA0001):
     def __init__(self,n_samples,n_machines,processing_time,due_date,weigths):
-        self._n_samples = None
-        self._n_machines = None
-        self._processing_time = None
-        self._self.due_date = None
-        self._weights = None
-        self._poulation = None
-        self._percent_cross = None
-        self._percent_mutation = None
-        self._fitness = None
+        self._n_samples = self.set_n_samples(n_samples)
+        self._n_machines = self._set_n_machines(n_machines)
+        self._processing_time = self._set_processing_time(processing_time)
+        self._due_date = self._set_due_date(due_date)
+        self._weights = self._set_weights(weights)
+        self._poulation = self._set_population()
+        self._percent_cross = self._set_percent_cross(percent_cross)
+        self._percent_mutation = self._set_percent_mutation(percent_mutation)
+        self._fitness = self._set_fitness(fitness)
 
 
     def set_n_samples(self,n_samples):
-        if self._poulation != None:
-            self._n_samples = n_samples
+        if self._poulation == None:
+            return n_samples
         else:
             if n_samples < self._n_samples.shape[0]:
                 self._n_samples = self._n_samples[0:n_samples]
@@ -31,50 +31,50 @@ class Single_Machine(PermutationA0001,CrossA0001,MutationA0001):
                 None
 
     def _set_n_machines(self,n_machines):
-        if self._poulation != None:
-            self._n_machines = n_machines
+        if self._poulation == None:
+            return n_machines
         else:
             None
 
     def _set_processing_time(self,processing_time):
-        if self._poulation != None:
-            self._processing_time = processing_time
+        if self._poulation == None:
+            return processing_time
         else:
             None
 
     def _set_due_date(self,due_date):
-        if self._poulation != None:
-            self._due_date = due_date
+        if self._poulation == None:
+            return due_date
         else:
             None
     
     def _set_weights(self,weights):
-        if self._poulation != None:
-            self._weights = weights
+        if self._poulation == None:
+            return weights
         else:
             None
     
-    def _set_population(self,population):
-        if self._poulation != None:
-            self._poulation = population
+    def _set_population(self):
+        if self._poulation == None:
+            None
         else:
             None
 
     def _set_percent_cross(self,percent_cross):
         if self._poulation != None:
-            None
+            return percent_cross
         else:
             None
 
     def _set_percent_mutation(self,percent_mutation):
-        if self._poulation != None:
-            None
+        if self._poulation == None:
+            return percent_mutation
         else:
             None
     
     def _set_fitness(self,fitness):
-        if self._poulation != None:
-            None
+        if self._poulation == None:
+            return fitness
         else:
             None
 

c74fae894f55d0bee50a019d28c06bbb259fa5cb
mandalarotation <mandalarotation@gmail.com>
2020-09-08 13:43:05 +0000

    Configuracion rapida del setup, despues se debe definir una imagen docker para la facil uso de cuda y no tener que lidiar con la instalacion manual de dependencias y librerias de enlace dinamico

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(
+        name="gsc",
+        version="1.1.1",
+        description="Backend analitica de datos SGC",
+        author="Jean Carlo Jimenez Giraldo",
+        author_email="mandalarotation@gmail.com",
+        url="https://github.com/mandalarotation/GenSchedulingCuda-GSC",
+        packages=[
+                "gsc",
+                "gsc.kernels"],
+        scripts=[],
+        install_requires=["numpy",
+                          "cupy",
+                          "numba"]

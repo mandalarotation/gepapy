2da81c34f00f0704c4f3f23eb29d947f49bce60f
mandalarotation <mandalarotation@gmail.com>
2020-10-01 22:12:08 +0000

    los tres metodos completamente funcionales
    Todavia hace falta implementar ciertas excepciones para evitar que el usuario ejecute estructuras mal definidas

--- gsc/single_machine.py
+++ gsc/single_machine.py
@@ -7,14 +7,14 @@ import math
 from gsc.operations import Operations
 
 class Single_Machine(Operations):
-    def __init__(self,n_samples=10,n_jobs=8,processing_time=None,machine_sequence=None,due_date=None,weights=None,percent_cross=0.5,percent_intra_cross=0.5,percent_mutation=0.5,percent_intra_mutation=0.1,percent_migration=0.5,percent_selection=0.1,fitness_type="E_Lw"):
+    def __init__(self,n_samples=10,n_jobs=8,processing_time=None,due_date=None,weights=None,percent_cross=0.5,percent_intra_cross=0.5,percent_mutation=0.5,percent_intra_mutation=0.1,percent_migration=0.5,percent_selection=0.1,fitness_type="E_Lw"):
         self._n_samples = self.set_n_samples(n_samples)
         self._n_jobs = self._set_n_jobs(n_jobs)
         self._n_machines = 1
         self._n_operations = 1
         self._fitness_type = self._set_fitness_type(fitness_type)
         self._processing_time = self._set_processing_time(processing_time)
-        self._machine_sequence = self._set_machine_sequence(machine_sequence)
+        self._machine_sequence = cp.expand_dims(cp.zeros(n_jobs,dtype=cp.float32),axis=1)
         self._due_date = self._set_due_date(due_date)
         self._weights = self._set_weights(weights)        
         self._percent_cross = self.set_percent_cross(percent_cross)
--- script.py
+++ script.py
@@ -11,9 +11,8 @@ start_time = time.time()
 T_ = cp.expand_dims(cp.array([10,10,13,4,9,4,8,15,7,1,9,3,15,9,11,6,5,14,18,3],dtype=cp.float32),axis=1)
 d_ = cp.array([50,38,49,12,20,105,73,45,6,64,15,6,92,43,78,21,15,50,150,99],dtype=cp.float32)
 w_ = cp.array([10,5,1,5,10,1,5,10,5,1,5,10,10,5,1,10,5,5,1,5],dtype=cp.float32)
-M_ = cp.expand_dims(cp.zeros(20,dtype=cp.float32),axis=1)
 
-p = Single_Machine(n_samples=100000,n_jobs=20,processing_time=T_,machine_sequence=M_,due_date=d_,weights=w_,percent_cross=0.8,percent_mutation=0.8,percent_migration=0.1,fitness_type="E_LTw")
+p = Single_Machine(n_samples=100000,n_jobs=20,processing_time=T_,due_date=d_,weights=w_,percent_cross=0.8,percent_mutation=0.8,percent_migration=0.1,fitness_type="E_LTw")
 
 
 fitness = []

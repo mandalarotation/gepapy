fde4927aeda49798f0e0847ab047d47b097ba4a0
mandalarotation <mandalarotation@gmail.com>
2020-11-15 16:12:38 -0500

    exceptions

--- gen_scheduling_cuda/exceptions/operations.py
+++ gen_scheduling_cuda/exceptions/operations.py
@@ -67,9 +67,18 @@ class Check:
             :type n_samples: int
             :rtype: Optional[Union[None, int]]
             """
+            if n_samples < 2:
+                raise SetException(
+                    "n_samples -> {} ; expected -> n_samples >= 2".format(
+                        type(n_samples)
+                    )
+                )
+
             if type(n_samples) != int:
                 raise SetException(
-                    "type(n_samples) -> {} ; expected -> int".format(type(n_samples))
+                    "type(n_samples) -> {} ; expected -> <class 'int'>".format(
+                        type(n_samples)
+                    )
                 )
             return f(self, n_samples)
 
@@ -92,7 +101,12 @@ class Check:
             :type n_jobs: int
             :rtype: Optional[Union[None, int]]
             """
-            print("estoy decorada")
+            if type(n_jobs) != int:
+                raise SetException(
+                    "type(n_jobs) -> {} ; expected -> <class 'int'>".format(
+                        type(n_jobs)
+                    )
+                )
             return f(self, n_jobs)
 
         return cast(TFun, wrapper)
@@ -114,7 +128,12 @@ class Check:
             :type n_machines: int
             :rtype: Optional[Union[None, int]]
             """
-            print("estoy decorada")
+            if type(n_machines) != int:
+                raise SetException(
+                    "type(n_machines) -> {} ; expected -> <class 'int'>".format(
+                        type(n_machines)
+                    )
+                )
             return f(self, n_machines)
 
         return cast(TFun, wrapper)
@@ -136,7 +155,12 @@ class Check:
             :type n_operations: int
             :rtype: Optional[Union[None, int]]
             """
-            print("estoy decorada")
+            if type(n_operations) != int:
+                raise SetException(
+                    "type(n_operations) -> {} ; expected -> <class 'int'>".format(
+                        type(n_operations)
+                    )
+                )
             return f(self, n_operations)
 
         return cast(TFun, wrapper)
@@ -158,7 +182,28 @@ class Check:
             :type fitness_type: str
             :rtype: Optional[Union[None, str]]
             """
-            print("estoy decorada")
+            fitness_list = [
+                "E_C",
+                "E_L",
+                "E_LT",
+                "E_U",
+                "E_Lw",
+                "E_LTw",
+                "E_Uw",
+                "max_C",
+            ]
+            if not (fitness_type in fitness_list):
+                raise SetException(
+                    "type(fitness_type) -> {} ; expected -> ['E_C','E_L','E_LT','E_U','E_Lw','E_LTw','E_Uw','max_C']".format(  # noqa: E501
+                        type(fitness_type)
+                    )
+                )
+            if type(fitness_type) != str:
+                raise SetException(
+                    "type(fitness_type) -> {} ; expected -> <class 'str'>".format(
+                        type(fitness_type)
+                    )
+                )
             return f(self, fitness_type)
 
         return cast(TFun, wrapper)
@@ -180,7 +225,19 @@ class Check:
             :type percent_cross: float
             :rtype: Optional[Union[None, float]]
             """
-            print("estoy decorada")
+            if percent_cross <= 0 or percent_cross >= 1:
+                raise SetException(
+                    "type(percent_cross) -> {} ; expected -> Interval[(0,1)]".format(
+                        type(percent_cross)
+                    )
+                )
+
+            if type(percent_cross) != float:
+                raise SetException(
+                    "type(percent_cross) -> {} ; expected -> <class 'float'>".format(
+                        type(percent_cross)
+                    )
+                )
             return f(self, percent_cross)
 
         return cast(TFun, wrapper)
@@ -202,7 +259,19 @@ class Check:
             :type percent_intra_cross: float
             :rtype: Optional[Union[None, float]]
             """
-            print("estoy decorada")
+            if percent_intra_cross <= 0 or percent_intra_cross >= 1:
+                raise SetException(
+                    "type(percent_intra_cross) -> {} ; expected -> Interval[(0,1)]".format(
+                        type(percent_intra_cross)
+                    )
+                )
+
+            if type(percent_intra_cross) != float:
+                raise SetException(
+                    "type(percent_intra_cross) -> {} ; expected -> <class 'float'>".format(
+                        type(percent_intra_cross)
+                    )
+                )
             return f(self, percent_intra_cross)
 
         return cast(TFun, wrapper)
@@ -224,7 +293,18 @@ class Check:
             :type percent_mutation: float
             :rtype: Optional[Union[None, float]]
             """
-            print("estoy decorada")
+            if percent_mutation <= 0 or percent_mutation >= 1:
+                raise SetException(
+                    "type(percent_mutation) -> {} ; expected -> Interval[(0,1)]".format(
+                        type(percent_mutation)
+                    )
+                )
+            if type(percent_mutation) != float:
+                raise SetException(
+                    "type(percent_mutation) -> {} ; expected -> <class 'float'>".format(
+                        type(percent_mutation)
+                    )
+                )
             return f(self, percent_mutation)
 
         return cast(TFun, wrapper)
@@ -248,7 +328,18 @@ class Check:
             :type percent_intra_mutation: float
             :rtype: Optional[Union[None, float]]
             """
-            print("estoy decorada")
+            if percent_intra_mutation <= 0 or percent_intra_mutation >= 1:
+                raise SetException(
+                    "type(percent_intra_mutation) -> {} ; expected -> Interval[(0,1)]".format(
+                        type(percent_intra_mutation)
+                    )
+                )
+            if type(percent_intra_mutation) != float:
+                raise SetException(
+                    "type(percent_intra_mutation) -> {} ; expected -> <class 'float'>".format(
+                        type(percent_intra_mutation)
+                    )
+                )
             return f(self, percent_intra_mutation)
 
         return cast(TFun, wrapper)
@@ -270,7 +361,18 @@ class Check:
             :type percent_migration: float
             :rtype: Optional[Union[None, float]]
             """
-            print("estoy decorada")
+            if percent_migration <= 0 or percent_migration >= 1:
+                raise SetException(
+                    "type(percent_migration) -> {} ; expected -> Interval[(0,1)]".format(
+                        type(percent_migration)
+                    )
+                )
+            if type(percent_migration) != float:
+                raise SetException(
+                    "type(percent_migration) -> {} ; expected -> <class 'float'>".format(
+                        type(percent_migration)
+                    )
+                )
             return f(self, percent_migration)
 
         return cast(TFun, wrapper)
@@ -292,7 +394,18 @@ class Check:
             :type percent_selection: float
             :rtype: Optional[Union[None, float]]
             """
-            print("estoy decorada")
+            if percent_selection <= 0 or percent_selection >= 1:
+                raise SetException(
+                    "type(percent_selection) -> {} ; expected -> Interval[(0,1)]".format(
+                        type(percent_selection)
+                    )
+                )
+            if type(percent_selection) != float:
+                raise SetException(
+                    "type(percent_selection) -> {} ; expected -> <class 'float'>".format(
+                        type(percent_selection)
+                    )
+                )
             return f(self, percent_selection)
 
         return cast(TFun, wrapper)
@@ -317,7 +430,12 @@ class Check:
             :type fitness: cp.core.core.ndarray
             :rtype: Optional[Union[None, cp.core.core.ndarray]]
             """
-            print("estoy decorada")
+            if type(fitness) != cp.core.core.ndarray:
+                raise SetException(
+                    "type(fitness) -> {} ; expected -> <class 'cp.core.core.ndarray'>".format(
+                        type(fitness)
+                    )
+                )
             return f(self, fitness)
 
         return cast(TFun, wrapper)
@@ -341,7 +459,12 @@ class Check:
             :type population: Optional[Union[cp.core.core.ndarray, None]]
             :rtype: Optional[Union[None, cp.core.core.ndarray]]
             """
-            print("estoy decorada")
+            if type(population) != cp.core.core.ndarray:
+                raise SetException(
+                    "type(population) -> {} ; expected -> <class 'cp.core.core.ndarray'>".format(
+                        type(population)
+                    )
+                )
             return f(self, population)
 
         return cast(TFun, wrapper)
@@ -366,7 +489,16 @@ class Check:
             :type processing_time: Optional[Union[list, np.ndarray, cp.core.core.ndarray]]
             :rtype: cp.core.core.ndarray
             """
-            print("estoy decorada")
+            if not (
+                type(processing_time) == list
+                or type(processing_time) == np.ndarray
+                or type(processing_time) == cp.core.core.ndarray
+            ):
+                raise SetException(
+                    "type(processing_time) -> {} ; expected -> <class 'Optional[Union[list, np.ndarray, cp.core.core.ndarray]]'>".format(  # noqa: E501
+                        type(processing_time)
+                    )
+                )
             return f(self, processing_time)
 
         return cast(TFun, wrapper)
@@ -391,7 +523,16 @@ class Check:
             :type machine_sequence: Optional[Union[list, np.ndarray, cp.core.core.ndarray]]
             :rtype: cp.core.core.ndarray
             """
-            print("estoy decorada")
+            if not (
+                type(machine_sequence) == list
+                or type(machine_sequence) == np.ndarray
+                or type(machine_sequence) == cp.core.core.ndarray
+            ):
+                raise SetException(
+                    "type(machine_sequence) -> {} ; expected -> <class 'Optional[Union[list, np.ndarray, cp.core.core.ndarray]]'>".format(  # noqa: E501
+                        type(machine_sequence)
+                    )
+                )
             return f(self, machine_sequence)
 
         return cast(TFun, wrapper)
@@ -415,7 +556,16 @@ class Check:
             :type due_date: Optional[Union[list, np.ndarray, cp.core.core.ndarray]]
             :rtype: cp.core.core.ndarray
             """
-            print("estoy decorada")
+            if not (
+                type(due_date) == list
+                or type(due_date) == np.ndarray
+                or type(due_date) == cp.core.core.ndarray
+            ):
+                raise SetException(
+                    "type(due_date) -> {} ; expected -> <class 'Optional[Union[list, np.ndarray, cp.core.core.ndarray]]'>".format(  # noqa: E501
+                        type(due_date)
+                    )
+                )
             return f(self, due_date)
 
         return cast(TFun, wrapper)
@@ -439,7 +589,16 @@ class Check:
             :type weights: Optional[Union[list, np.ndarray, cp.core.core.ndarray]]
             :rtype: cp.core.core.ndarray
             """
-            print("estoy decorada")
+            if not (
+                type(weights) == list
+                or type(weights) == np.ndarray
+                or type(weights) == cp.core.core.ndarray
+            ):
+                raise SetException(
+                    "type(weights) -> {} ; expected -> <class 'Optional[Union[list, np.ndarray, cp.core.core.ndarray]]'>".format(  # noqa: E501
+                        type(weights)
+                    )
+                )
             return f(self, weights)
 
         return cast(TFun, wrapper)
@@ -473,7 +632,14 @@ class Check:
             :type percent_selection: float
             :rtype: None
             """
-            print("estoy decorada")
+            if (
+                percent_cross > percent_selection
+                or percent_mutation > percent_selection
+                or percent_migration > percent_selection
+            ):
+                raise SetException(
+                    "False ; expected ->  True  where percent_cross < percent_selection and percent_mutation < percent_selection and percent_migration < percent_selection"  # noqa: E501
+                )
             return f(
                 self,
                 percent_cross,
